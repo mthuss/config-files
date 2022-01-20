d578529fb0be6853542e5cc718341354a77a5dea
Moriel <86256436+mthuss@users.noreply.github.com>
2022-01-19 21:34:14 -0300

    Added script to automate the install process

---
+++ install.sh
@@ -0,0 +1,6 @@
+[[ ! -d $HOME/.vimundo ]] && mkdir $HOME/.vimundo
+[[ ! -d $HOME/.vim ]] && mkdir $HOME/.vim
+[[ ! -d $HOME/.vim/colors ]] && mkdir $HOME/.vim/colors
+[[ ! -f $HOME/.vim/colors/gruvbox.vim ]] && wget https://raw.githubusercontent.com/morhetz/gruvbox/master/colors/gruvbox.vim -P $HOME/.vim/colors/
+[[ -f $HOME/.vimrc ]] mv .vimrc .vimrc.old
+[[ ! -f $HOME/.vimrc ]] mv .vimrc $HOME
