e753874d565d15c98c5a8ef813c4960da8f5d045
Moriel <86256436+mthuss@users.noreply.github.com>
2022-01-27 11:16:42 -0300

    Silence wget output and add a check

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 [[ ! -d $HOME/.vimundo ]] && mkdir $HOME/.vimundo
 [[ ! -d $HOME/.vim ]] && mkdir $HOME/.vim
 [[ ! -d $HOME/.vim/colors ]] && mkdir $HOME/.vim/colors
-[[ ! -f $HOME/.vim/colors/gruvbox.vim ]] && wget https://raw.githubusercontent.com/morhetz/gruvbox/master/colors/gruvbox.vim -P $HOME/.vim/colors/
+[[ ! -f $HOME/.vim/colors/gruvbox.vim ]] && wget -q https://raw.githubusercontent.com/morhetz/gruvbox/master/colors/gruvbox.vim -P $HOME/.vim/colors/
 [[ -f $HOME/.vimrc ]] && mv $HOME/.vimrc $HOME/.vimrc.old
 [[ ! -f $HOME/.vimrc ]] && mv .vimrc $HOME
+[[ -f $HOME/.vimrc && -f $HOME/.vim/colors/gruvbox.vim && -d $HOME/.vimundo ]] && echo "Install successful" || echo "Something went wrong"
