228aa33134087989fe95d03bfdc138abc5944284
Moriel <86256436+mthuss@users.noreply.github.com>
2022-01-27 10:58:53 -0300

    Fix syntax

--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@
 [[ ! -d $HOME/.vim ]] && mkdir $HOME/.vim
 [[ ! -d $HOME/.vim/colors ]] && mkdir $HOME/.vim/colors
 [[ ! -f $HOME/.vim/colors/gruvbox.vim ]] && wget https://raw.githubusercontent.com/morhetz/gruvbox/master/colors/gruvbox.vim -P $HOME/.vim/colors/
-[[ -f $HOME/.vimrc ]] && mv .vimrc .vimrc.old
+[[ -f $HOME/.vimrc ]] && mv $HOME/.vimrc $HOME/.vimrc.old
 [[ ! -f $HOME/.vimrc ]] && mv .vimrc $HOME
