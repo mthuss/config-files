d9489587b096cc7a897881db8fba2aac8dc21334
Moriel <86256436+mthuss@users.noreply.github.com>
2022-01-27 11:37:32 -0300

    Added check to see if .vimrc is to be replaced

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@
 [[ ! -d $HOME/.vim ]] && mkdir $HOME/.vim
 [[ ! -d $HOME/.vim/colors ]] && mkdir $HOME/.vim/colors
 [[ ! -f $HOME/.vim/colors/gruvbox.vim ]] && wget -q https://raw.githubusercontent.com/morhetz/gruvbox/master/colors/gruvbox.vim -P $HOME/.vim/colors/
-[[ -f $HOME/.vimrc ]] && mv $HOME/.vimrc $HOME/.vimrc.old
+[[ -f $HOME/.vimrc ]] && cmp -s .vimrc $HOME/.vimrc && echo "No changes made to .vimrc" || mv .vimrc $HOME/.vimrc
 [[ ! -f $HOME/.vimrc ]] && mv .vimrc $HOME
 [[ -f $HOME/.vimrc && -f $HOME/.vim/colors/gruvbox.vim && -d $HOME/.vimundo ]] && echo "Install successful" || echo "Something went wrong"
