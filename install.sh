1071a603784f2fae1877a30d617d17aa121469ed
Moriel <86256436+mthuss@users.noreply.github.com>
2022-01-27 12:17:27 -0300

    Fixed checks

--- install.sh
+++ install.sh
@@ -2,6 +2,10 @@
 [[ ! -d $HOME/.vim ]] && mkdir $HOME/.vim
 [[ ! -d $HOME/.vim/colors ]] && mkdir $HOME/.vim/colors
 [[ ! -f $HOME/.vim/colors/gruvbox.vim ]] && wget -q https://raw.githubusercontent.com/morhetz/gruvbox/master/colors/gruvbox.vim -P $HOME/.vim/colors/
-[[ -f $HOME/.vimrc ]] && cmp -s .vimrc $HOME/.vimrc && echo "No changes made to .vimrc" || mv $HOME/.vimrc $HOME/.vimrc.old
-[[ ! -f $HOME/.vimrc ]] && mv .vimrc $HOME
+
+if [[ -f $HOME/.vimrc ]]
+then
+	cmp -s .vimrc $HOME/.vimrc && echo "No changes made to .vimrc" || mv $HOME/.vimrc $HOME/.vimrc.old
+fi
+[[ ! -f $HOME/.vimrc ]] && cp .vimrc $HOME
 [[ -f $HOME/.vimrc && -f $HOME/.vim/colors/gruvbox.vim && -d $HOME/.vimundo ]] && echo "Install successful" || echo "Something went wrong"
